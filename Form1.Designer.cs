d12f9dfc08d4c4e98315c595494d89c1ec900eaf
BobVoit <63784248+BobVoit@users.noreply.github.com>
2021-08-03 12:32:47 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Matrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.matrixView = new System.Windows.Forms.DataGridView();
+            this.sortIncrease = new System.Windows.Forms.Button();
+            this.sortWaning = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matrixView)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // matrixView
+            // 
+            this.matrixView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.matrixView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.matrixView.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.matrixView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrixView.Location = new System.Drawing.Point(363, 79);
+            this.matrixView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.matrixView.Name = "matrixView";
+            this.matrixView.ReadOnly = true;
+            this.matrixView.RowHeadersWidth = 51;
+            this.matrixView.RowTemplate.Height = 24;
+            this.matrixView.Size = new System.Drawing.Size(628, 444);
+            this.matrixView.TabIndex = 1;
+            // 
+            // sortIncrease
+            // 
+            this.sortIncrease.BackColor = System.Drawing.Color.RoyalBlue;
+            this.sortIncrease.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sortIncrease.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sortIncrease.Location = new System.Drawing.Point(13, 239);
+            this.sortIncrease.Margin = new System.Windows.Forms.Padding(4);
+            this.sortIncrease.Name = "sortIncrease";
+            this.sortIncrease.Size = new System.Drawing.Size(343, 63);
+            this.sortIncrease.TabIndex = 2;
+            this.sortIncrease.Text = "Возрастание";
+            this.sortIncrease.UseVisualStyleBackColor = false;
+            this.sortIncrease.Click += new System.EventHandler(this.sortIncrease_Click);
+            // 
+            // sortWaning
+            // 
+            this.sortWaning.BackColor = System.Drawing.Color.RoyalBlue;
+            this.sortWaning.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sortWaning.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sortWaning.Location = new System.Drawing.Point(13, 140);
+            this.sortWaning.Margin = new System.Windows.Forms.Padding(4);
+            this.sortWaning.Name = "sortWaning";
+            this.sortWaning.Size = new System.Drawing.Size(343, 63);
+            this.sortWaning.TabIndex = 3;
+            this.sortWaning.Text = "Убывание";
+            this.sortWaning.UseVisualStyleBackColor = false;
+            this.sortWaning.Click += new System.EventHandler(this.sortWaning_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(102, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сортировка";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.Filter = "Text Files | *.txt";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.справкаToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(65, 24);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(166, 26);
+            this.toolStripMenuItem2.Text = "Открыть";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(166, 26);
+            this.toolStripMenuItem3.Text = "Сохранить";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(166, 26);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(313, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Сортировка строк матрицы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1067, 534);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sortWaning);
+            this.Controls.Add(this.sortIncrease);
+            this.Controls.Add(this.matrixView);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Сортировка строк матрицы";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.matrixView)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView matrixView;
+        private System.Windows.Forms.Button sortIncrease;
+        private System.Windows.Forms.Button sortWaning;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Matrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<List<int>> matrix = new List<List<int>>();
+
+        // Преобразует массив строк в массив чисел
+        private int[] arrayStringToInt(string[] arr)
+        {
+            try
+            {
+                return Array.ConvertAll(arr, s => int.Parse(s));
+            } catch (FormatException e)
+            {
+                return null;
+            }
+        }
+
+        private int getMaxLength()
+        {
+            int maxLength = 0;
+            foreach (List<int> list in matrix)
+            {
+                if (list.Count > maxLength)
+                {
+                    maxLength = list.Count;
+                }
+            }
+            return maxLength;
+        }
+
+        private string introduceMatrixInString()
+        {
+            string result = "";
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                result += String.Join(" ", matrix[i]) + (i < (matrix.Count - 1) ? "\n" : "");
+            }
+            return result;
+        }
+
+        private void fillDataGridView()
+        {
+            int width = matrix[0].Count;
+            int height = matrix.Count;
+
+            matrixView.ColumnCount = width;
+
+            for (int i = 0; i < height; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(matrixView);
+
+                for (int j = 0; j < width; j++)
+                {
+                    row.Cells[j].Value = matrix[i][j];
+                }
+
+                matrixView.Rows.Add(row);
+            }
+        }
+
+        private void sortIncreaseMatrixRows()
+        {
+            foreach (List<int> line in matrix)
+            {
+                line.Sort();
+            }
+        }
+
+        private void sortWaningMatrixRows()
+        {
+            foreach (List<int> line in matrix)
+            {
+                line.Sort((a, b) => b.CompareTo(a));
+            }
+        }
+
+        // Добавление недоставющих элементов матрице
+        // для выравнивании количества элементов матрицы 
+        private void matrixAlignment()
+        {
+            int maxLength = getMaxLength();
+
+            foreach (List<int> list in matrix)
+            {
+                while (list.Count != maxLength) {
+                    list.Add(0);
+                }
+            }
+        }
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                matrixView.Rows.Clear();
+                matrix.Clear();
+                openFileDialog.ShowDialog();
+                string filename = openFileDialog.FileName;
+                string readFile = File.ReadAllText(filename);
+                char[] separators = new char[] { '\n', '\r' };
+                string[] lines = readFile.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in lines)
+                {
+                    string[] row = line.Split(' ');
+                    int[] intRow = arrayStringToInt(row);
+                    List<int> newRow = intRow.OfType<int>().ToList();
+                    matrix.Add(newRow); 
+                }
+                matrixAlignment();
+
+                fillDataGridView();
+            } catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        // События FormClosing для формы. Отвечает за вызов сообщения 
+        // с подтверждением закрытия программы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Выйти из приложения?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        // Кнопка "Возрастание"
+        private void sortIncrease_Click(object sender, EventArgs e)
+        {
+            if (matrix.Count != 0)
+            {
+                matrixView.Rows.Clear();
+                sortIncreaseMatrixRows();
+                fillDataGridView();
+            }
+        }
+
+        // Кнопка "Убывание"
+        private void sortWaning_Click(object sender, EventArgs e)
+        {
+            if (matrix.Count != 0)
+            {
+                matrixView.Rows.Clear();
+                sortWaningMatrixRows();
+                fillDataGridView();
+            }
+        }
+
+        private void saveMatrix_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog.FileName;
+            System.IO.File.WriteAllText(filename, introduceMatrixInString());
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пограмма предназначена для обработки входных матриц из файлов формата txt. В приложении можно отсортировать строки матрицы по возрастанию/убыванию, а затем записать данные в файл", "Справка");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                matrixView.Rows.Clear();
+                matrix.Clear();
+                openFileDialog.ShowDialog();
+                string filename = openFileDialog.FileName;
+                string readFile = File.ReadAllText(filename);
+                char[] separators = new char[] { '\n', '\r' };
+                string[] lines = readFile.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in lines)
+                {
+                    string[] row = line.Split(' ');
+                    int[] intRow = arrayStringToInt(row);
+                    List<int> newRow = intRow.OfType<int>().ToList();
+                    matrix.Add(newRow);
+                }
+                matrixAlignment();
+
+                fillDataGridView();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Входные данные неверные!");
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog.FileName;
+            System.IO.File.WriteAllText(filename, introduceMatrixInString());
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Пограмма предназначена для обработки входных матриц из файлов формата txt. В приложении можно отсортировать строки матрицы по возрастанию/убыванию, а затем записать данные в файл", "Справка");
+        }
+    }
+}
